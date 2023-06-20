e2540c30ddecaf312a85ef15301269b9c98c41bc
Gregory Vincic <gregoryvse@gmail.com>
2023-06-20 09:27:50 +0200

    Introduce loan

--- eda.go
+++ eda.go
@@ -3,7 +3,15 @@
 Expenses data format
 
 # comments, empty lines are ignored
+# special comments, [expense|loan] signals the subsequent line formats
+
+# expense
 int/[y|m] tag1 ... tagn
+
+# loan
+# date left original interest installment tags
+20220228 686453 720000     3.34        599 lån Bolån_1 3994 15 72194 gemensamt 3år
+20220228 668800 720000     3.39        700 lån Bolån_2 3994 15 72208 gemensamt 4år
 
 */
 package eda
@@ -13,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -39,7 +48,10 @@ func NewScanner(r io.Reader) *Scanner {
 type Scanner struct {
 	lineno int
 	s      *bufio.Scanner
+	loan   bool
 }
+
+var re = regexp.MustCompile(`\s+`)
 
 func (s *Scanner) Scan() (*Entry, error) {
 next:
@@ -49,42 +61,74 @@ next:
 		return nil, io.EOF
 	}
 	line := strings.TrimSpace(s.s.Text())
+	// remove multiple spaces
+	line = re.ReplaceAllString(line, " ")
+	// toggle loan|expense parsing
+	if strings.HasPrefix(line, "# loan") {
+		s.loan = true
+	}
+	if strings.HasPrefix(line, "# expense") {
+		s.loan = false
+	}
 	if strings.HasPrefix(line, "#") || len(line) == 0 {
 		goto next
 	}
-	line = strings.Replace(line, "/", " ", 1) // optional '/'
+	line = strings.Replace(line, "/", " ", 1) // in expense entries
 
 	parts := strings.Split(line, " ")
-	// parse amount
-	amount, err := strconv.Atoi(parts[0])
-	if err != nil {
-		return nil, fmt.Errorf("invalid amount on line %v: %s", s.lineno, line)
-	}
 
-	var e Entry
-	e.Amount = amount
-	e.Period = parts[1]
-	e.Tags = parts[2:]
-	return &e, nil
+	if s.loan {
+
+		// todo
+		var e Entry
+		return &e, nil
+	} else {
+		// parse amount
+		amount, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return nil, fmt.Errorf("invalid amount on line %v: %s", s.lineno, line)
+		}
+
+		var e Entry
+		e.amount = amount
+		e.Period = parts[1]
+		e.Tags = parts[2:]
+		return &e, nil
+	}
 }
 
 // ----------------------------------------
 
 type Entry struct {
-	Amount int
+	amount int
 	Period string
 	Tags   []string
 }
 
+type Loan struct {
+	Date        string
+	Left        int
+	Orig        int
+	Interest    float64
+	installment int
+	Tags        []string
+}
+
+func (l *Loan) Monthly() int{
+	// todo
+	return 0
+}
+
+
 func (e *Entry) String() string {
-	return fmt.Sprintf("%v/%s %s", e.Amount, string(e.Period), strings.Join(e.Tags, " "))
+	return fmt.Sprintf("%v/%s %s", e.amount, string(e.Period), strings.Join(e.Tags, " "))
 }
 
 func (e *Entry) Monthly() int {
 	switch e.Period {
 	case "y":
-		return e.Amount / 12
+		return e.amount / 12
 	default:
-		return e.Amount
+		return e.amount
 	}
 }
